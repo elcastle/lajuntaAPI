5f38b58dc31c7d6a16b8de2715fdc60460300bbd
JorgeCastilla <w1zard333@outlook.com>
2022-07-03 19:21:02 -0400

    configuration

--- conexion/ConexionPDO.php
+++ conexion/ConexionPDO.php
@@ -1,8 +1,8 @@
 <?php 
 class ConexionPDO{
-    private $dsn = 'mysql:host=localhost;dbname=laJunta';
-    private $username = 'root';
-    private $password = 'root';
+    private $dsn = 'mysql:host=sql108.epizy.com;dbname=epiz_32094821_lajunta';
+    private $username = 'epiz_32094821';
+    private $password = 'IllE8AamHT9J3b';
     public $mysql;
 
     function __construct() {
